1d73adfbcf723b9456812935574638eee2245dea
Mike Galbraith <umgwanakikbuti@gmail.com>
2015-07-14 17:39:50 +0200

    sched/fair: Beef up wake_wide()
    Josef Bacik reported that Facebook sees better performance with their
    1:N load (1 dispatch/node, N workers/node) when carrying an old patch
    to try very hard to wake to an idle CPU.  While looking at wake_wide(),
    I noticed that it doesn't pay attention to the wakeup of a many partner
    waker, returning 1 only when waking one of its many partners.
    
    Correct that, letting explicit domain flags override the heuristic.
    
    While at it, adjust task_struct bits, we don't need a 64-bit counter.
    
    Tested-by: Josef Bacik <jbacik@fb.com>
    Signed-off-by: Mike Galbraith <umgwanakikbuti@gmail.com>
    [ Tidy things up. ]
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: kernel-team<Kernel-team@fb.com>
    Cc: morten.rasmussen@arm.com
    Cc: riel@redhat.com
    Link: http://lkml.kernel.org/r/1436888390.7983.49.camel@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1284,12 +1284,9 @@ struct task_struct {
 #ifdef CONFIG_SMP
 	struct llist_node wake_entry;
 	int on_cpu;
-#ifdef CONFIG_THREAD_INFO_IN_TASK
-	unsigned int cpu;	/* current CPU */
-#endif
-	struct task_struct *last_wakee;
-	unsigned long wakee_flips;
+	unsigned int wakee_flips;
 	unsigned long wakee_flip_decay_ts;
+	struct task_struct *last_wakee;
 
 	int wake_cpu;
 #endif
